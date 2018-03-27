5a152c72efe75e1513967abf28f1217164d54bd2
Julian Kohlman <juliankohlman@gmail.com>
2018-03-27 18:27:45 -0400

    All steps done except 6-7

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -6,142 +6,143 @@
  * Class for simulating a simple Computer (CPU & memory)
  */
 class CPU {
+  /**
+   * Initialize the CPU
+   */
+  constructor(ram) {
+    this.ram = ram;
 
-    /**
-     * Initialize the CPU
-     */
-    constructor(ram) {
-        this.ram = ram;
+    this.reg = new Array(8).fill(0); // General-purpose registers R0-R7
 
-        this.reg = new Array(8).fill(0); // General-purpose registers R0-R7
+    // Special-purpose registers
+    this.reg.PC = 0; // Program Counter
+  }
 
-        // Special-purpose registers
-        this.reg.PC = 0; // Program Counter
+  /**
+   * Store value in memory address, useful for program loading
+   */
+  poke(address, value) {
+    this.ram.write(address, value);
+  }
+
+  /**
+   * Starts the clock ticking on the CPU
+   */
+  startClock() {
+    const _this = this;
+
+    this.clock = setInterval(() => {
+      _this.tick();
+    }, 1); // 1 ms delay == 1 KHz clock == 0.000001 GHz
+  }
+
+  /**
+   * Stops the clock
+   */
+  stopClock() {
+    clearInterval(this.clock);
+  }
+
+  /**
+   * ALU functionality
+   *
+   * The ALU is responsible for math and comparisons.
+   *
+   * If you have an instruction that does math, i.e. MUL, the CPU would hand
+   * it off to it's internal ALU component to do the actual work.
+   *
+   * op can be: ADD SUB MUL DIV INC DEC CMP
+   */
+  alu(op, regA, regB) {
+    switch (op) {
+      case 'MUL':
+        return this.reg[regA] = (regA * regB);
+        break;
     }
+  }
 
-    /**
-     * Store value in memory address, useful for program loading
-     */
-    poke(address, value) {
-        this.ram.write(address, value);
-    }
+  /**
+   * Advances the CPU one cycle
+   */
+  tick() {
+    // opcodes
+    const LDI = 0b10011001;
+    const HLT = 0b00000001;
+    const PRN = 0b01000011;
+    const ADD = 0b10101000;
+    const MUL = 0b10101010;
 
-    /**
-     * Starts the clock ticking on the CPU
-     */
-    startClock() {
-        const _this = this;
+    // IR >>> 6 + 1<-- for opcode
+    //           ^^ operands + 1 10 in binary is 2 | 2 + 1 = 3
+    let IR = this.ram.read(this.reg.PC)
 
-        this.clock = setInterval(() => {
-            _this.tick();
-        }, 1); // 1 ms delay == 1 KHz clock == 0.000001 GHz
-    }
+    let opA = this.ram.read(this.reg.PC + 1);
+    let opB = this.ram.read(this.reg.PC + 2);
 
-    /**
-     * Stops the clock
-     */
-    stopClock() {
-        clearInterval(this.clock);
-    }
+    let handleLDI = () => this.reg[opA] = opB;
+    let handlePRN = () => console.log(this.reg[opA]);
+    let handleHLT = () => this.stopClock();
+    let handleMUL = () => this.alu(MUL, opA, opB);
+    let table = {}
 
-    /**
-     * ALU functionality
-     *
-     * The ALU is responsible for math and comparisons.
-     *
-     * If you have an instruction that does math, i.e. MUL, the CPU would hand
-     * it off to it's internal ALU component to do the actual work.
-     *
-     * op can be: ADD SUB MUL DIV INC DEC CMP
-     */
-    alu(op, regA, regB) {
-        switch (op) {
-            case 'MUL':
-                // !!! IMPLEMENT ME
-                break;
-        }
-    }
+    table[LDI] = handleLDI;
+    table[HLT] = handleHLT;
+    table[PRN] = handlePRN;
+    table[MUL] = handleMUL;
 
-    /**
-     * Advances the CPU one cycle
-     */
-    tick() {
-        // CREATE BRANCH TABLE OR HASH
-        const LDI = '10011001';
-        const PRN = '01000011';
-        const HLT = '00000001';
+    let handler = table[IR]; // add default unknown instruction msg.
 
-        // setTable[LDI] = execution of the instruction
-            //  poke(opA, opB)
-        // Load the instruction register (IR--can just be a local variable here)
-        // from the memory address pointed to by the PC. (I.e. the PC holds the
-        // index into memory of the next instruction.)
+    handler();
+    // EXECUTE INSTRUCTIONS
+    // switch (IR) {
+    //   case HLT:
+    //     this.stopClock();
+    //     break;
 
-        // !!! IMPLEMENT ME
+    //   case LDI:
+    //     this.reg[opA] = opB;
+    //     break;
 
-        let IR = this.ram.read(this.reg.PC.toString(2))
-        console.log(IR)
+    //   case PRN:
+    //     console.log(this.reg[opA]);
+    //     break;
 
-        // Debugging output
-        console.log(`${this.reg.PC}: ${IR.toString(2)}`);
+    //   default:
+    //     console.log("Unknown instruction: " + IR.toString(2));
+    //     this.stopClock();
+    //     break;
+    // }
 
-        // Get the two bytes in memory _after_ the PC in case the instruction
-        // needs them.
+    // INCREMENT PC REGISTER
+    this.reg.PC += (IR >>> 6) + 1
 
-        // !!! IMPLEMENT ME
+    // Load the instruction register (IR--can just be a local variable here)
+    // from the memory address pointed to by the PC. (I.e. the PC holds the
+    // index into memory of the next instruction.)
 
-        let opA = this.ram.read(this.reg.PC + 1);
-        let opB = this.ram.read(this.reg.PC + 2);
-        console.log(opA, opB)
+    // !!! IMPLEMENT ME
 
-        // Execute the instruction. Perform the actions for the instruction as
-        // outlined in the LS-8 spec.
+    // Debugging output
+    console.log('4', `${this.reg.PC}: ${IR.toString(2)}`);
 
-        // !!! IMPLEMENT ME
+    // Get the two bytes in memory _after_ the PC in case the instruction
+    // needs them.
 
-        // Increment the PC register to go to the next instruction. Instructions
-        // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
-        // instruction byte tells you how many bytes follow the instruction byte
-        // for any particular instruction.
+    // !!! IMPLEMENT ME
 
-        // !!! IMPLEMENT ME
-    }
+    // instruction handlers
+    // Execute the instruction. Perform the actions for the instruction as
+    // outlined in the LS-8 spec.
+
+    // !!! IMPLEMENT ME
+
+    // Increment the PC register to go to the next instruction. Instructions
+    // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
+    // instruction byte tells you how many bytes follow the instruction byte
+    // for any particular instruction.
+
+    // !!! IMPLEMENT ME
+  }
 }
-
-/* Pseudocode, not valid JS:
-
-const LDI = 0b10011001; // From the LS-8 spec
-const HLT = 0b00000001;
-
-function handle_LDI() { ... }
-function handle_HLT() { ... }
-
-branchTable[LDI] = handle_LDI;
-branchTable[HLT] = handle_HLT;
-
-let IR = this.mem.read(this.reg.PC); // Fetch instruction
-let handler = branchTable[IR]; // Look up handler in branch table
-
-handler(); // Call it
-
- etc.// Pseudocode, not valid JS:
-
-const LDI = 0b10011001; // From the LS-8 spec
-const HLT = 0b00000001;
-
-function handle_LDI() { ... }
-function handle_HLT() { ... }
-
-branchTable[LDI] = handle_LDI;
-branchTable[HLT] = handle_HLT;
-
-let IR = this.mem.read(this.reg.PC); // Fetch instruction
-let handler = branchTable[IR]; // Look up handler in branch table
-
-handler(); // Call it
-etc.
-*/
-
-
 
 module.exports = CPU;
